bb94964cedb10f462d06dab27f1baaf6d9a69f6c
Sayooj <dev.sayooj@gmail.com>
2021-07-15 13:36:49 +0530

    Initial Commit

---
+++ BankingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankingApp
+//
+//  Created by Sayooj Krishnan  on 15/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
